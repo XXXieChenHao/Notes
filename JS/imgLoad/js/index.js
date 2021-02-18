3243f8c2e98c9c44fd0ee56d9027393352dea6fd
xch <xie.chenhao@ruisiming.com.cn>
2021-02-18 18:39:33 +0800

    <feat>: lazyload and debounce

---
+++ JS/imgLoad/js/index.js
@@ -0,0 +1,59 @@
+; (function (win, doc) {
+  // 获取容器
+  var oImgList = doc.getElementsByClassName('J_imgList')[0],
+    data = JSON.parse(doc.getElementById('J_data').innerHTML),  // 获取 data
+    imgTpl = doc.getElementById('J_imgTpl').innerHTML,  // 获取模板
+    oImg = doc.getElementsByClassName('list-img');
+
+  var init = function () {
+    oImgList.innerHTML = renderList(data)
+    bindEvent();
+  }
+
+  // 绑定事件
+  function bindEvent() {
+      // window.onload = window.onscroll = throttle(imgLazyLoad(oImg), 500);
+    window.onload = window.onscroll = function () {
+      imgLazyLoad(oImg)();
+    }
+  }
+
+  // 渲染列表
+  function renderList(data) {
+    var list = '';
+
+    data.forEach(function (elem) {
+      list += imgTpl.replace(/{{(.*?)}}/g, function (node, key) {
+        return {
+          img: elem.img,
+          name: elem.name
+        }[key];
+        // 返回一个对象 并且返回对象中 key 对应的的 value
+      })
+    });
+    return list
+  }
+  init()
+})(window, document);
+
+// 懒加载
+function imgLazyLoad(image) {
+  var imgLen = image.length,
+      n = 0;
+  return function () {
+    // 闭包 n 不会被清除掉
+    var cHeight = document.documentElement.clientHeight,  // 可视区域高度
+        sTop = document.documentElement.scrollTop || document.body.scroll, // 滚动高度
+        imgItem;
+
+    for (var i = n; i < imgLen; i++) {
+      imgItem = image[i];
+      if(imgItem.offsetTop < cHeight + sTop) {
+        imgItem.src = imgItem.getAttribute('data-src')
+        imgItem.removeAttribute('data-src')
+        n++;
+      }
+    }
+  }
+
+}
