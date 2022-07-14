b2cb3874a8fcd79d04db9248df8768270986de0b
xiechenhao <xiechenhao@uniev.com>
2022-07-14 17:53:45 +0800

    <bugfix>: 归并

--- JS/排序/mergeSort.js
+++ JS/排序/mergeSort.js
@@ -1,35 +1,35 @@
 
-// const merge = (left, right) => {
-//   let result = [];
+const merge = (left, right) => {
+  let result = [];
 
-//   while (left.length && right.length) {
-//     // 如果左边小于或等于右侧，就将左侧放入结果中，泛指取右侧的值放入结果
-//     let temp = left[0] <= right[0] ? left.shift() : right.shift()
-//     result.push(temp);
-//   }
-//   // 如果left 无值则
-//   if (!left.length) {
-//     result = result.concat(right);
-//     right = []
-//   }
-//   if (!right.length) {
-//     result = result.concat(left);
-//     left = []
-//   }
-//   return result
-// }
+  while (left.length && right.length) {
+    // 如果左边小于或等于右侧，就将左侧放入结果中，泛指取右侧的值放入结果
+    let temp = left[0] <= right[0] ? left.shift() : right.shift()
+    result.push(temp);
+  }
+  // 如果left 无值则
+  if (!left.length) {
+    result = result.concat(right);
+    right = []
+  }
+  if (!right.length) {
+    result = result.concat(left);
+    left = []
+  }
+  return result
+}
 
-// const mergeSort = arr => {
-//   // 一个值直接返回即可
-//   if (arr.length < 2) return arr;
-//   // 取出中间索引
-//   let middle = Math.floor(arr.length / 2);
-//   // 拆分为两个子数组
-//   let left = arr.slice(0, middle);
-//   let right = arr.slice(middle);
-//   let res = merge(mergeSort(left), mergeSort(right));
-//   return res
-// }
+const mergeSort = arr => {
+  // 一个值直接返回即可
+  if (arr.length < 2) return arr;
+  // 取出中间索引
+  let middle = Math.floor(arr.length / 2);
+  // 拆分为两个子数组
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+  let res = merge(mergeSort(left), mergeSort(right));
+  return res
+}
 
 
 // // const merge = (a, b) => {
@@ -69,39 +69,3 @@
 
 // let a = mergeSort([1, 5, 2, 8, 0, 9, 2, 1, 7])
 // console.log(a)
-
-const sort = (arr) => {
-  return inplaceSort(arr, 0, arr.length)
-};
-
-const inplaceSort = (arr, start, end) => {
-  if (end - start <= 1) {
-    return arr;
-  }
-  let middle = parseInt((start + end) / 2);
-  inplaceSort(arr, start, middle);
-  inplaceSort(arr, middle, end);
-  inplaceMerge(arr, start, middle, end);
-  return arr;
-};
-
-const inplaceMerge = (arr, start, middle, end) => {
-  let i = start,
-    k = middle;
-  while (i < middle && k < end) {
-    let w = 0;
-    while (arr[i] <= arr[k] && i < middle) {
-      i += 1;
-    }
-    while (arr[i] >= arr[k] && k < end) {
-      k += 1;
-      w += 1;
-    }
-    let part = arr.splice(k - w, w);
-    arr.splice(i, 0, ...part);
-    i += w;
-    middle += w;
-  }
-  return arr;
-};
-console.log(sort([3, 5, 2, 7, 1, 9]))
